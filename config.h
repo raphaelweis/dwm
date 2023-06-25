4141c3e5372205e4e7033bdcd4c5729c1f67d691
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-06-25 22:26:45 +0200

    successfully merged statuspadding patch

--- config.h
+++ config.h
@@ -5,6 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 5;        /* vertical padding for statusbar */
 static const char *fonts[]          = { "JetBrainsMono NL NF:size=12" };
 static const char dmenufont[]       = "JetBrainsMono NL NF:size=12";
 static const char col_gray1[]       = "#222222";
