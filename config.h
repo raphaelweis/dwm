bd272c778981568a85e3e1ff8cc1de3c02244a1e
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-03 22:20:29 +0200

    changed alpha values for bar and borders

--- config.h
+++ config.h
@@ -17,8 +17,8 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_orange[]      = "orange";
 /* static const char col_cyan[]        = "#005577"; */
-static const unsigned int baralpha = OPAQUE;
-static const unsigned int borderalpha = OPAQUE;
+static const unsigned int baralpha = 950;
+static const unsigned int borderalpha = 950;
 static const char *colors[][3]      = {
 	/*               fg         bg          border   */
 	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2 },
