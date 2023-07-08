79628a27e141cb08d167f7976cc8bc8b10f15e3d
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-07-08 13:57:26 +0200

    minor code style updates

--- config.h
+++ config.h
@@ -26,7 +26,7 @@ static const char *colors[][3]      = {
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
-  [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
 	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 	/* class          instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",         NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",      NULL,       NULL,       1 << 8,       0,           -1 },
-  { "Pavucontrol",  NULL,       NULL,       0,            1,           -1 },
+    { "Pavucontrol",  NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -53,11 +53,11 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
+	{ "[]=",  tile },    /* first entry is default */
+	{ "><>",  NULL },    /* no layout function means floating behavior */
+	{ "[M]",  monocle },
+ 	{ "[@]",  spiral },
+ 	{ "[\\]", dwindle },
 };
 
 /* key definitions */
